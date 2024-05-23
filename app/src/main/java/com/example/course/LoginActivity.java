b69e42be8cfaf2b1cba6542a069be37456796d73
molodoy1911 <komandin-a@bk.ru>
2024-05-23 22:18:33 +0300

    full login and registration

--- app/src/main/java/com/example/course/LoginActivity.java
+++ app/src/main/java/com/example/course/LoginActivity.java
@@ -14,6 +14,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etPassword;
     private Button btnLogin;
     private UserDAO userDAO;
+    private Button btnRegister, btnBack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnBack = findViewById(R.id.btnBack);
         userDAO = new UserDAO(this);
         userDAO.open();
 
@@ -45,6 +48,13 @@ public class LoginActivity extends AppCompatActivity {
                 } else {
                     Toast.makeText(LoginActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
--- app/src/main/java/com/example/course/UserActivity.java
+++ app/src/main/java/com/example/course/UserActivity.java
@@ -1,12 +1,27 @@
 package com.example.course;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UserActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        Button btnViewCars = findViewById(R.id.btnViewCars);
+        btnViewCars.setOnClickListener(v -> {
+            Intent intent = new Intent(UserActivity.this, CarsActivity.class);
+            startActivity(intent);
+        });
+
+        Button btnViewHelp = findViewById(R.id.btnViewHelp);
+        btnViewHelp.setOnClickListener(v -> {
+            Intent intent = new Intent(UserActivity.this, ViewHelpActivity.class);
+            startActivity(intent);
+        });
     }
 }
